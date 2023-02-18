f77a4e5a6ba16ef5c3fde6f728d06bcb9539b84c
efmomoh25@gmail.com <efmomoh25@gmail.com>
2023-02-18 08:31:56 -0800

    Writing a code that prints a number from zero through nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints number 10 statring from 0 to 9
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+	return (0);
+}
