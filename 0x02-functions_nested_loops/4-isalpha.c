a0b3b4464db4035261c2afba47b72f2e77b82d4d
efmomoh25@gmail.com <efmomoh25@gmail.com>
2023-02-21 16:59:13 -0800

    Write a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check alphabet char upper or lower case
+ * @c: check if it is upper or lower char
+ * Return: 1 if it is lower char or upper char, else return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
