46e18747532eb229ce450db9e3993914407897ba
efmomoh25@gmail.com <efmomoh25@gmail.com>
2023-02-23 15:54:00 -0800

    Write a function that multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,29 @@
 #include "main.h"
 /**
  * mul - check code
- * @a: check code
- * @b: check code
- * Return: 0 always
+ * @a: check code, first
+ * @b: check code, second
+ * Return: 0 products
  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
 
+Task 3. 3 - print_numbers.c
+
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; fl++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
