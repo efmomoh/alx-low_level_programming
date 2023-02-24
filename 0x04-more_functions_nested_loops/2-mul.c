68823c7eb8b8703d5d06dca107b7064e9440b64e
efmomoh25@gmail.com <efmomoh25@gmail.com>
2023-02-23 16:51:08 -0800

    Write a function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -9,21 +9,3 @@ int mul(int a, int b)
 {
 	return (a * b);
 }
-
-Task 3. 3 - print_numbers.c
-
-#include "main.h"
-/**
- * print_numbers - print from 0 to 9
- * Return: nothing
- */
-void print_numbers(void)
-{
-	int ch;
-
-	for (ch = 48; ch < 58; fl++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
-}
